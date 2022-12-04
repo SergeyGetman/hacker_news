1fc64253267b3d782ef64988b129fd89871b7fe6
Sergey Getman <sergeygetman2020@gmail.com>
2022-12-04 02:16:52 +0200

    change styles

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,10 +6,11 @@ import {listSelector} from "../redux/selector";
 const Home = () => {
 
     const news = useSelector(listSelector)
+    console.log('this is NEWS', news)
     return (
         <div>
             <div className={classes.title}>
-                <h2>{`${news?.title}`}</h2>
+                <h2>WELCOME ON A HACKER FORUM...</h2>
             </div>
         </div>
     );
